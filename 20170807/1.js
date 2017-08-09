74e8d9de6ba4affded5fc8a58b2b005f8771fbf4
yechang <215372535@qq.com>
2017-08-09 21:07:02 +0800

    8.9

---
+++ 20170807/1.js
@@ -0,0 +1,19 @@
+
+
+
+window.onload = function(){
+    
+
+
+    (function(){
+
+        window.ucai = window.ucai || {};
+
+        function getAge(){
+            return 18;
+        }
+    })()
+
+
+    window.ucai.getAge()
+}
